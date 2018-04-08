9d543f246231fed7f1978adc5789aef9410deba7
Daniel Demidko <DanielDemidko@gmail.com>
2018-04-08 15:38:41 +1000

    Delete Console.h

--- Calculator/Console.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-namespace Console
-
-{
-    std::vector<std::string> ReadExpression();
-    void PrintResult(const std::vector<std::string> &expr, const double res);
-}
